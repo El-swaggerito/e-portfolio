2347e61b3c51a1e8da6765a64ae7b670ac2cef75
El-swaggerito <95658387+El-swaggerito@users.noreply.github.com>
2024-01-20 18:54:49 +0100

    E-portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,8 +7,6 @@ import preview from "../assets/portfolio/code.png";
 import quote from "../assets/portfolio/quote.png";
 import weather from "../assets/portfolio/weather.png";
 import instagram from "../assets/portfolio/instagram.png";
-import Modal from "./Modal";
-import { useState } from "react";
 
 const Portfolio = () => {
   const portfolios = [
@@ -17,54 +15,63 @@ const Portfolio = () => {
       src: calculator,
       demo: "https://react-calculator-900.pages.dev/",
       code: "https://github.com/El-swaggerito/FCC-react-calculator.git",
+      lang: "HTML, CSS, JavaScript",
     },
     {
       id: 2,
       src: currency,
       demo: "https://ac2ea42e.currency-converter-7kr.pages.dev/converter.html",
       code: "https://github.com/El-swaggerito/Currency-Converter.git",
+      lang: "HTML, CSS, JavaScript",
     },
     {
       id: 3,
       src: drum,
       demo: "https://fcc-drum-machine.pages.dev/",
       code: "https://github.com/El-swaggerito/FCC-drum-machine.git",
+      lang: "ReactTs, Css",
     },
     {
       id: 4,
       src: ping,
       demo: "https://34424a0f.ping-pong-1mc.pages.dev/game.html",
       code: "https://github.com/El-swaggerito/Ping-Pong.git",
+      lang: "HTML, CSS, JavaScript",
     },
     {
       id: 5,
       src: pomodoro,
       demo: "https://pomodoro-clock-6qj.pages.dev/",
       code: "https://github.com/El-swaggerito/FCC-25-5-Clock.git",
+      lang: "ReactTs, Css",
     },
     {
       id: 6,
       src: preview,
       demo: "https://markdown-previewer-7ur.pages.dev/",
       code: "https://github.com/El-swaggerito/FCC-markdown-previewer.git",
+      lang: "ReactTs, Css",
     },
     {
       id: 7,
       src: quote,
       demo: "https://fcc-random-quote-machine-henna.vercel.app/",
       code: "https://github.com/El-swaggerito/FCC-random-quote-machine.git",
+      lang: "ReactTs, Css",
     },
     {
       id: 8,
       src: weather,
       demo: "https://d498aec0.weather-app-e7a.pages.dev/weather.html",
       code: "https://github.com/El-swaggerito/Weather-App.git",
+      lang: "HTML, CSS, JavaScript",
     },
     {
       id: 9,
       src: instagram,
       demo: "https://instagram-clone-seven-delta.vercel.app/",
       code: "https://github.com/El-swaggerito/Instagram-clone.git",
+      lang: "ReactJs, Chakra UI, Firebase",
     },
   ];
 
@@ -83,7 +90,7 @@ const Portfolio = () => {
             <p className="py-6">Check out some of my work below</p>
           </div>
           <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-10 sm:px-0">
-            {portfolios.map(({ id, src, demo, code }) => (
+            {portfolios.map(({ id, src, demo, code, lang }) => (
               <div
                 key={id}
                 className="flex flex-col items-center justify-center shadow-md shadow-gray-600 rounded-lg"
@@ -107,6 +114,9 @@ const Portfolio = () => {
                     </button>
                   </a>
                 </div>
+                <p className="text-center text-gray-500 text-xs">
+                  This Project was built using {lang}{" "}
+                </p>
               </div>
             ))}
           </div>
